222e8aeedf51fad6e9579a19a8c8f0a84dcaf987
Md Ashrafujjaman Tutul <ashrafujjamantutul@gmail.com>
2025-02-12 15:29:01 +0600

    Fixed roomservice

--- src/service/RoomService.js
+++ src/service/RoomService.js
@@ -4,7 +4,7 @@ import { AppConfig } from "../config/AppConfig";
 const BASE_BACKEND_API=AppConfig.backendBaseApi;
 
 const getRoomWithRoomId = async (roomId) => {
-    const url = `${BASE_BACKEND_API}/api/v1/rooms/${1}`
+    const url = `${BASE_BACKEND_API}/api/v1/rooms/${roomId}`
     return await axios.get(url);
 }
 
